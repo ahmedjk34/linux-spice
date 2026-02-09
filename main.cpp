4c043c3c93f488709b8ef6be6f63196f3dab2697
ahmedjk34 <ahmedtaher212005@gmail.com>
2026-02-09 22:00:37 +0200

    This is gonna suck and be fun at the same time ;)

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Linux Spice People\n";
+    cout << "Sick of AI, and honestly not the best at low level langauges\n";
+    cout << "This is meant to suck.";
+    exit(0);
+}
